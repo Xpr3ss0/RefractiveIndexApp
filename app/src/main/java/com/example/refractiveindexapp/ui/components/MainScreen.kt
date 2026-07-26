6c3d78bd224634c6814b96ec65f922f64ca3755a
Julius Selg <julius.selg@gmail.com>
2026-07-27 00:07:06 +0200

    minor changes

--- app/src/main/java/com/example/refractiveindexapp/ui/components/MainScreen.kt
+++ app/src/main/java/com/example/refractiveindexapp/ui/components/MainScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.room.parser.Section
 import com.example.refractiveindexapp.database.Material
 import com.example.refractiveindexapp.parsing.Catalogue
 import com.example.refractiveindexapp.parsing.MaterialGatherer
@@ -37,6 +40,12 @@ fun MainScreen(
 
             Text(viewModel.selectedPage?.dataPath ?: "no page selected")
             Text(viewModel.currentMaterial?.references ?: "no material loaded")
+            // VerticalDivider(thickness = 10.dp)
+            Text(viewModel.currentMaterial?.dispersionModel?.formulaType?.toString() ?: "no dispersion coefficients available.")
+            Text(viewModel.currentMaterial?.dispersionModel?.coefficients ?: "...")
+            Text(viewModel.currentMaterial?.tabulatedData?.type ?: "no tabulated data available.")
+            Text(viewModel.currentMaterial?.tabulatedData?.content ?: "...")
+            //VerticalDivider(thickness = 10.dp)
         }
 
     }
